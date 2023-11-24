cb48c5bbad0d314b4d80aeb539a455234c2633e0
AlphaMale <55350092+anjm1020@users.noreply.github.com>
2023-11-24 17:17:32 +0900

    feat: mariaDB 연동 및 데이터 조회 (#4)
    * feat: Mybatis, mariaDB 의존성 추가
    
    * feat: db, logger configure
    
    * feat: movie 도메인 구현
    
    * feat: movieVO 구현

---
+++ src/main/java/com/jaemin/dao/MovieDAOImpl.java
@@ -0,0 +1,25 @@
+package com.jaemin.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.jaemin.vo.MovieVO;
+
+@Repository
+public class MovieDAOImpl implements MovieDAO {
+
+	@Inject
+	private SqlSession sqlSession;
+	private static final String Namespace = "com.jaemin.mybatis.sql.test";
+	
+	@Override
+	public List<MovieVO> selectMovie() throws Exception {
+		
+		return sqlSession.selectList(Namespace+".selectMovie");
+	}
+
+}
