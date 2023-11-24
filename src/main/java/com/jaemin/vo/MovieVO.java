a248a7999f1dbe9c7075c0dae9a61a25f8997f30
jaemin <anjm1020@gmail.com>
2023-11-24 17:16:41 +0900

    feat: movieVO 구현

---
+++ src/main/java/com/jaemin/vo/MovieVO.java
@@ -0,0 +1,43 @@
+package com.jaemin.vo;
+
+public class MovieVO {
+	private Long movie_id;
+	private String movie_name;
+	private String director;
+	private String type;
+	private String moviecol;
+	public Long getMovie_id() {
+		return movie_id;
+	}
+	public void setMovie_id(Long movie_id) {
+		this.movie_id = movie_id;
+	}
+	public String getMovie_name() {
+		return movie_name;
+	}
+	public void setMovie_name(String movie_name) {
+		this.movie_name = movie_name;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getMoviecol() {
+		return moviecol;
+	}
+	public void setMoviecol(String moviecol) {
+		this.moviecol = moviecol;
+	}
+	
+	
+	
+	
+}
